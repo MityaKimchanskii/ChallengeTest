2b56dcbb81fcf49900752ad390b3d919fe603d7d
Dmitrii Kim <mitya_kim@icloud.com>
2022-05-04 12:29:20 -0700

    commit

--- Rapptr iOS Test/View Controllers/AnimationViewController.swift
+++ Rapptr iOS Test/View Controllers/AnimationViewController.swift
@@ -12,7 +12,7 @@ class AnimationViewController: UIViewController {
     var location = CGPoint(x: 0, y: 0)
     
     // MARK: - IBOutlets
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var fadeinButton: UIButton!
     
     // MARK: - Lifecycle
@@ -23,17 +23,17 @@ class AnimationViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func didPressFade(_ sender: Any) {
-        let imageHide = imageView.alpha == 1
+        let imageHide = logoImageView.alpha == 1
         
         if imageHide {
             UIView.animate(withDuration: 1.5, delay: 0.0, options: .curveEaseOut, animations: {
-                self.imageView.alpha = 0.0
+                self.logoImageView.alpha = 0.0
             }, completion: nil)
             fadeinButton.setTitle("FADE IN", for: .normal)
            
         } else {
             UIView.animate(withDuration: 1.5, delay: 0.0, options: .curveEaseOut, animations: {
-                self.imageView.alpha = 1.0
+                self.logoImageView.alpha = 1.0
             }, completion: nil)
             fadeinButton.setTitle("FADE OUT", for: .normal)
         }
@@ -42,12 +42,12 @@ class AnimationViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch: UITouch = touches.first!
         location = touch.location(in: self.view)
-        imageView.center = location
+        logoImageView.center = location
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch: UITouch = touches.first!
         location = touch.location(in: self.view)
-        imageView.center = location
+        logoImageView.center = location
     }
 }
